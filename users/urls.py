bfec7bf12ce3281b863676c41f3da3923dacdea3
A1909bek <aa2004bek@gmail.com>
2025-04-10 16:44:45 +0500

    next commit

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('register/',views.RegisterView.as_view(),name='register'),
     path('password-change/',views.PasswordChangeView.as_view(),name='password_change'),
     path('password-change-done/',views.PasswordChangeDoneView.as_view(),name='password_change_done'),
+    path('password-reset/',views.PasswordResetView.as_view(),name='password_reset'),
+    path('password-reset-done/',views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('password-reset-complete/',views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
 ]
--- users/views.py
+++ users/views.py
@@ -7,6 +7,10 @@ from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth import logout
 from users.tasks import send_email_task
+from users.models import CustomUser
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
 
 # Create your views here.
 
@@ -97,6 +101,79 @@ class PasswordChangeView(View):
 class PasswordChangeDoneView(View):
     def get(self,request):
         return render(request,'registration/password_change_done.html')
+    
+
+class PasswordResetView(View):
+    def get(self,request):
+        return render(request,'registration/password_reset.html')
+    
+    def post(self,request):
+        email = request.POST.get('email')
+        user = CustomUser.objects.filter(email=email).first()
+        if user:
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            token = default_token_generator.make_token(user)
+            reset_link = request.build_absolute_uri(f"/users/password-reset-confirm/{uid}/{token}/")
+
+            send_email_task.delay(
+                subject='Parolni tiklash',
+                message=f"Parolingizni tiklash uchun xavola:{reset_link}",
+                recipient_list=[user.email]
+            )
+        return redirect('password_reset_done')
+
+class PasswordResetDoneView(View):
+    def get(self,request):
+        return render(request,'registration/password_reset_done.html')
+    
+
+class PasswordResetConfirmView(View):
+    def get(self,request,uidb64,token):
+        context = {
+            'uidb64':uidb64,
+            'token':token
+        }
+        return render(request,'registration/password_reset_confirm.html',context)
+    
+    def post(self,request,uidb64,token):
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        if password1 and password2 and password1 != password2:
+            messages.error(request,'Parollar mos kelmadi')
+            return redirect(request.path)
+        try:
+            uid = urlsafe_base64_decode(uidb64).decode()
+            user = CustomUser.objects.get(pk=uid)
+        except (TypeError,ValueError,OverflowError):
+            messages.error(request,'Havola yaroqsiz')
+            return redirect('password_reset')
+        
+        if default_token_generator.check_token(user,token):
+            user.set_password(password1)
+            user.save()
+            send_email_task.delay(
+                subject="Parol tiklandi",
+                message="Parolingiz muvaffaqiyatli tiklandi",
+                recipient_list=[user.email]
+            )
+            return redirect('password_reset_complete')
+        else:
+            messages.error(request,'Havola yaroqsiz yoki eskirgan')
+            return redirect('password_reset')
+        
+
+class PasswordResetCompleteView(View):
+    def get(self,request):
+        return render(request,'registration/password_reset_complete.html')
+    
+        
+    
+        
+
+
+    
+    
+
     
     
 
